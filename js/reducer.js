04c87af82be5521625283e0d2d1bd89704501d69
Agnieszka Brown <abrown2012@icloud.com>
2021-07-29 15:13:02 -0400

    commit tree cf42076e21f794f63e32de1a7d9674bee6423770

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,18 @@
-function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
-  }
-}
+let state = {count: 0};
 
-let state = {count: 0}
+function changeState(state, action){
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
+  }
+
+function dispatch(action){
+  state = changeState(state, action)
+  return state
+}
 let action = {type: 'INCREASE_COUNT'}
 
 changeState(state, action)
